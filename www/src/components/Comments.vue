20703efa82366da2e95ef1b1de90c787f4bcd335
Christian Tran <christiantran04@gmail.com>
2018-06-03 22:53:39 -0600

    comment info created but not arranged

--- www/src/components/Comments.vue
+++ www/src/components/Comments.vue
@@ -1,6 +1,41 @@
 <template>
     <div>
 
+
+<!-- CREATE COMMENT -->
+          <!-- Modal -->
+<button type="button" class='btn btn-link' data-toggle="modal" data-target="#createBoardModal">Create Comment</button>
+          <div class="modal fade" id="createCommentModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Create a New Comment</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <form v-on:submit.prevent="newComment">
+                    <div class="form-group">
+                      <input type="text" name="commentBody" v-model="
+                          comment.body" class="form-control" id="formGroupExampleInput" placeholder="Comment" required>
+                    </div>
+                    <div class="form-group">
+                      <input type="text" name="commentDescription" v-model="
+                              comment.description" class="form-control" id="formGroupExampleInput" placeholder="Description">
+                    </div>
+                  </form>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                  <button type="button" @click="newComment" class="btn btn-primary" data-dismiss="modal">Create Comment</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+<!-- VIEW COMMENT -->
 <div>
       <div class="card" v-for="comment in comments" style="width: 20rem;">
         <div class="card-body">
@@ -8,10 +43,16 @@
           <h6 class="card-subtitle mb-2 text-muted"></h6>
           <p>{{comment.description}}</p>
           <router-link :to="'/Comments/'+comment._id">View Comment</router-link>
-          <a href="#" class="card-link" @click="removeComment(comment)">Delete Comment</a>
         </div>
       </div>
     </div>
+
+
+<!-- DELETE COMMENT -->
+<a href="#" class="card-link" @click="removeComment(comment)">Delete Comment</a>
+
+
+
 
     </div>
 </template>
